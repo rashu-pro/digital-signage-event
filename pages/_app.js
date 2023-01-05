de04b03d5e36f0a5aa121e65ef82a4690c260b9f
rashu-pro <rashu.pro@gmail.com>
2023-01-05 20:28:31 +0600

    home page html integrated so far

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import 'normalize.css/normalize.css'
+import '../styles/style.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
