792aa3411296d1d37c41d57a5de8adb0781b56d9
rashu-pro <rashu.pro@gmail.com>
2023-02-14 19:08:03 +0600

    ticker api has been changed, credit text size decreased

--- pages/EventDetails.js
+++ pages/EventDetails.js
@@ -19,34 +19,7 @@ export default function EventDetails() {
   const { data, error } = useSWR('https://secure-api.net/api/v1/company-announcement?slug='+getSlug(), fetcher, { refreshInterval: refreshInterval })
   if(error) return <p className='text-center'> Failed to load... </p>
   if(!data) return <p className='text-center'>loading...</p>
-  // console.log(typeof data.announcments);
-
-  // return;
-  // const data = {
-  //   "heading_top": "PLEASE DONATE FOR THE MULTIPURPOSE HALL",
-  //   "status": 1,
-  //   "announcements": [
-  //     {
-  //       "title": "ICS THANKSGIVING PICNIC",
-  //       "image": "https://test.skifftech.com/media/jummah-namaz.jpg",
-  //       "date": "16/01/2023",
-  //       "description": "<div class=\"ds-event-body\"><p class=\"m-0 mb-1\">Assalam-O-Alaikum Community Members,</p><p>Please note next week on Friday November 25, 2022</p><p class=\"text-underline\">Jummah Khutbah topic is:</p><p class=\"text-uppercase\"><span class=\"text-bold\">A LIFE OF GIVING THANKS</span><br> <span class=\"text-small text-normalcase\">By IMAM SAFI KHAN</span></p><p class=\"pt-2\">There is no shortage of blessings to be thankful for. What is the importance of thankfulness? How should we express it and what effects should it have on us? We reflect on blessings and the true manifestation of thankfulness and gratitude. Saying Alhamdolilah.</p></div>"
-  //     },
-  //     {
-  //       "title": "A Life of Giving Thanks",
-  //       "image": "https://test.skifftech.com/media/happy-thanks-giving.gif",
-  //       "date": "16/01/2023",
-  //       "description": "<div class=\"ds-event-body\"><p class=\"m-0 mb-1\">Assalam-O-Alaikum Community Members,</p><p>Please note next week on Friday November 25, 2022</p><p class=\"text-underline\">Jummah Khutbah topic is:</p><p class=\"text-highlight text-center text-uppercase\"><span class=\"text-bold\">A LIFE OF GIVING THANKS</span><br> <span class=\"text-small text-normalcase\">By IMAM SAFI KHAN</span></p><p class=\"pt-2\">There is no shortage of blessings to be thankful for. What is the importance of thankfulness? How should we express it and what effects should it have on us? We reflect on blessings and the true manifestation of thankfulness and gratitude. Saying Alhamdolilah.</p></div>"
-  //     },
-  //     {
-  //       "title": "ICS THANKSGIVING PICNIC",
-  //       "image": "",
-  //       "date": "Saturday, November 26",
-  //       "description": "<div><p class=\"pb-2\">The ICS social team has planned a fun filled picnic for al ages Insha Allah. Please join us in celebrating community spirit and friendship at the masjid.</p> <h4>Assalamu Alaikum Dear Community Members,</h4><h3>ICS is exited for the upcoming picnic!</h3>Insha Allah On:<p><strong>Date: </strong> Saturday, November 26</p><p><strong>Time: </strong> 12PM - 5PM</p><p><strong>Location: </strong> Islamic Center of Southlake</p><p>The ICS social team has planned a fun filled picnic for al ages Insha Allah. Please join us in celebrating community spirit and friendship at the masjid.</p><p>We look forward to seeing you all there with families!</p></div>"
-  //     }
-  //   ]
-  // }
-
+  
   return (
     <>
       <div className="ds-event-wrapper-body h-full">
--- pages/components/ticker.js
+++ pages/components/ticker.js
@@ -3,16 +3,23 @@ import getSlug from "./slug";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
 export default function MarqueeText(){
-  const { data, error } = useSWR('https://secure-api.net/api/v1/company-prayer?slug='+getSlug(), fetcher)
+  const { data, error } = useSWR('https://secure-api.net/api/v1/company-announcement?slug='+getSlug(), fetcher)
   if(error) return <p className='m-0 text-center'> Failed to load... </p>
   if(!data) return <p className='m-0 text-center'>loading...</p>
 
   return (
     <>
-      {data.events.map((data, index)=>(
-        <div key={index} className="me-2 marquee-single">
-          <p className="m-0">{data.description} </p>
+      {data.announcments.map((data, index)=>(
+        <>
+        {data.smallDetails!==null?(
+          <div key={index} className="me-2 marquee-single">
+          <p className="m-0">{data.smallDetails} </p>
         </div>
+        ):(
+          <>
+          </>
+        )}
+        </>
       ))}
     </>
   )
